5f3780c13ec870714098cbcd5ca57f9b1051fe19
Kumar Saksham <everydaycodings@gmail.com>
2023-01-24 02:57:38 +0530

    streamlit deployment success

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ if user_bomber_select == bomber_options[0]:
     country_code = st.number_input("Enter Your Country Code Without + sign: ", step=1)
     phone_number = st.number_input("Enter The Phone Number: ", step=1)
     sms_count = st.number_input("Enter the number of messages you want to send (max: 500): ", step=1, min_value=1, max_value=500)
-    delay = st.number_input("Enter the delay time: (min: 5)", step=1, min_value=1)
+    delay = st.number_input("Enter the delay time: (min: 5)", step=1, min_value=5)
     
-    threads = st.number_input("Enter Number of Thread (max: 50)", step=1)
+    threads = st.number_input("Enter Number of Thread (max: 50)", step=1, min_value=1)
 
     if st.button("Bombard"):
         status = get_phone_info(country_code, phone_number)
@@ -41,7 +41,7 @@ elif user_bomber_select == bomber_options[1]:
     password = st.text_input("Enter your Gmail App Password: ")
     victime_email = st.text_input("Enter Your Victime Gmail Id: ")
     message = st.text_area("Enter your Message:")
-    message_relode = st.number_input("How many message you want to send to an individual: ", step=1)
+    message_relode = st.number_input("How many message you want to send to an individual: ", step=1, min_value=1)
 
     if st.button("Bombard"):
         result = email_bombing(victime_email, email, password, message, message_relode)
