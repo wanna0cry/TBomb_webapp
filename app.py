61a30bf28a35329fd27444ac40abc306704b5a8d
Kumar Saksham <everydaycodings@gmail.com>
2023-01-24 22:15:59 +0530

    added agreement statement

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ from helpers.smshelper import get_phone_info, selectnode
 from helpers.emailhelper import email_bombing
 from helpers.system_logs import platform_details
 
+
 max_limit = {"sms": 1000, "call": 15, "mail": 200}
+
+
+def agreement_accepting():
+
+    return st.caption("By using this webapp :green[you accept] that This application must not be used to cause :red[harm/discomfort/trouble] to :blue[others]. Developers of this webapp are not responsible for your actions." )
+
+
 
 st.sidebar.title("Bombers")
 
@@ -13,22 +21,24 @@ user_bomber_select = st.sidebar.selectbox("Select Your Bombing Technique: ", opt
 
 
 if user_bomber_select == bomber_options[0]:
-    st.write(user_bomber_select)
+    st.title(user_bomber_select)
 
-    country_code = st.number_input("Enter Your Country Code Without + sign: ", step=1)
+    country_code = st.number_input("Enter Your Country Code Without + sign: ", step=1, value=91)
     phone_number = st.number_input("Enter The Phone Number: ", step=1)
     sms_count = st.number_input("Enter the number of messages you want to send (max: 500): ", step=1, min_value=1, max_value=500)
     delay = st.number_input("Enter the delay time: (min: 5)", step=1, min_value=5)
     
-    threads = st.number_input("Enter Number of Thread (max: 50)", step=1, min_value=1)
+    threads = (sms_count//10) if (sms_count//10) > 0 else 1
 
-    if st.button("Bombard"):
+    agreement_accepting()
+
+    if st.button("Start Bombing"):
         status = get_phone_info(country_code, phone_number)
 
         if status["status"] != "valid":
             st.warning(status["message"])
         else:
-            result = selectnode(str(country_code), str(phone_number), sms_count, delay, threads)
+            result = selectnode(str(country_code), str(phone_number), sms_count, delay, int(threads))
             st.success(result["status"])
             st.json(result)
             st.json(platform_details())
@@ -43,7 +53,8 @@ elif user_bomber_select == bomber_options[1]:
     message = st.text_area("Enter your Message:")
     message_relode = st.number_input("How many message you want to send to an individual: ", step=1, min_value=1)
 
-    if st.button("Bombard"):
+
+    if st.button("Start Bombing"):
         result = email_bombing(victime_email, email, password, message, message_relode)
         st.success(result)
         st.json(platform_details())
